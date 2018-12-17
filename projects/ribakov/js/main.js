7b516edeb1f26324b72fc5a29f6f16edd95dc349
Тимур <timur@Timurs-iMac.local>
2018-12-17 09:34:32 +0500

    ribakov

--- projects/ribakov/js/main.js
+++ projects/ribakov/js/main.js
@@ -20,6 +20,28 @@
 
   $('.js-collapse').on('click', collapse);
 
+  $('.js-owl').owlCarousel({
+    dots: false,
+    responsive: {
+      0: {
+        items: 1
+      },
+      576: {
+        items: 1
+      },
+      768: {
+        items: 2
+      },
+      992: {
+        items: 3
+      },
+      1450: {
+        items: 4
+      }
+    }
+  });
+
+  // 1500:
   $('.js-bxslider').bxSlider({
     pager: false,
     touchEnabled: false,
