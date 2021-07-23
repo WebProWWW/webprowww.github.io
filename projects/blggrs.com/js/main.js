e3033884589a1ecc60565f6929c19ee6470a0ec0
Tim <v.timur8484@gmail.com>
2021-07-23 14:14:16 +0500

    blggrs.com

---
+++ projects/blggrs.com/js/main.js
@@ -0,0 +1,15 @@
+/*!
+ * @author Timur Valiyev
+ * @link https://webprowww.github.io
+ */
+"use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+(function () {
+  var MyClass;
+
+  MyClass = function MyClass() {
+    _classCallCheck(this, MyClass);
+  };
+}).call(void 0);
