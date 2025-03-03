00c58334683a718fbbef0cd5f3452743419db1a9
tim <WebProWWW@users.noreply.github.com>
2025-03-03 16:45:02 +0500

    tg-alxas main

---
+++ projects/tg-alxas/js/main.js
@@ -0,0 +1,13 @@
+// class User {
+//     private name: string
+//     private age: number
+//     constructor(name: string, age: number) {
+//         this.name = name
+//         this.age = age
+//     }
+//     public get info() : string {
+//         return `Name: ${this.name}, Age: ${this.age}`
+//     }
+// }
+// const user = new User('John', 30)
+// console.log(user.info) // Name: John, Age: 30
