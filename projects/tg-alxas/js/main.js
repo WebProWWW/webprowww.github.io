efcc1e9e38e41a634093f7416a683c40e48325f3
tim <WebProWWW@users.noreply.github.com>
2025-03-05 16:44:14 +0500

    tg app nav чать страниц

--- projects/tg-alxas/js/main.js
+++ projects/tg-alxas/js/main.js
@@ -18,17 +18,21 @@ var Tab = /** @class */ (function () {
 jQuery('[data-tab]').each(function (i, el) {
     new Tab(jQuery(el));
 });
-if (typeof window.isWebApp === "boolean" && window.isWebApp) {
-    var view = window.Telegram.WebApp.initDataUnsafe.start_param;
-    var currentView = window.appView;
-    if (typeof view === "string" && view !== '' && view !== currentView) {
-        $('body').load("".concat(view, "-app.html"));
-    }
-    // if (typeof view === "string" && view !== '') {
-    //     jQuery.loa
-    // }
-    // const view: string =  window.Telegram.WebApp.initDataUnsafe.start_param
-    // if (typeof view === "string" && view !== '') {
-    //     jQuery.loa
-    // }
-}
+$('[data-view]').on('click', function (e) {
+    var view = jQuery(e.currentTarget).data('view');
+    jQuery('body').load("".concat(view, ".html"));
+});
+// if (typeof window.isWebApp === "boolean" && window.isWebApp) {
+//     const view: string =  window.Telegram.WebApp.initDataUnsafe.start_param
+//     const currentView = window.appView
+//     if (typeof view === "string" && view !== '' && view !== currentView) {
+//         $('body').load(`${view}-app.html`)
+//     }
+//     // if (typeof view === "string" && view !== '') {
+//     //     jQuery.loa
+//     // }
+//     // const view: string =  window.Telegram.WebApp.initDataUnsafe.start_param
+//     // if (typeof view === "string" && view !== '') {
+//     //     jQuery.loa
+//     // }
+// }
