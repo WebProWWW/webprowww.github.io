e3c742a66846aaa8396e822fc5fc097e9df2c88e
tim <WebProWWW@users.noreply.github.com>
2025-03-05 13:09:59 +0500

     часть разделов

--- projects/tg-alxas/js/main.js
+++ projects/tg-alxas/js/main.js
@@ -1,13 +1,20 @@
-// class User {
-//     private name: string
-//     private age: number
-//     constructor(name: string, age: number) {
-//         this.name = name
-//         this.age = age
-//     }
-//     public get info() : string {
-//         return `Name: ${this.name}, Age: ${this.age}`
-//     }
-// }
-// const user = new User('John', 30)
-// console.log(user.info) // Name: John, Age: 30
+var Tab = /** @class */ (function () {
+    function Tab($tab) {
+        var _this = this;
+        this.$_tab = $tab;
+        this.$_btns = $tab.find('[data-tab-btn]');
+        this.$_contents = $tab.find('[data-tab-content]');
+        this.$_btns.on('click', function (e) {
+            var target = jQuery(e.currentTarget);
+            var id = target.data('tab-btn');
+            _this.$_btns.removeClass('active');
+            target.addClass('active');
+            _this.$_contents.removeClass('active');
+            jQuery(id).addClass('active');
+        });
+    }
+    return Tab;
+}());
+jQuery('[data-tab]').each(function (i, el) {
+    new Tab(jQuery(el));
+});
