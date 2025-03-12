a1ad942b6aa1b78ed3a73897e2da623f7c5d5b7d
tim <WebProWWW@users.noreply.github.com>
2025-03-12 11:20:38 +0500

    alxas all pages

--- projects/tg-alxas/js/main.js
+++ projects/tg-alxas/js/main.js
@@ -1,38 +1,35 @@
 var Tab = /** @class */ (function () {
     function Tab($tab) {
-        var _this = this;
-        this.$_tab = $tab;
-        this.$_btns = $tab.find('[data-tab-btn]');
-        this.$_contents = $tab.find('[data-tab-content]');
-        this.$_btns.on('click', function (e) {
+        var $btns = $tab.find('[data-tab-btn]');
+        var $contents = $tab.find('[data-tab-content]');
+        $btns.on('click', function (e) {
             var target = jQuery(e.currentTarget);
             var id = target.data('tab-btn');
-            _this.$_btns.removeClass('active');
+            $btns.removeClass('active');
             target.addClass('active');
-            _this.$_contents.removeClass('active');
+            $contents.removeClass('active');
             jQuery(id).addClass('active');
         });
     }
     return Tab;
 }());
-jQuery('[data-tab]').each(function (i, el) {
-    new Tab(jQuery(el));
+var Faq = /** @class */ (function () {
+    function Faq($faq) {
+        var $q = $faq.find('[data-faq-q]');
+        var $a = $faq.find('[data-faq-a]');
+        var $active = $faq.find('[data-faq-active]');
+        $q.on('click', function () {
+            $active.toggleClass('active');
+            $a.stop();
+            $a.slideToggle(300);
+        });
+    }
+    return Faq;
+}());
+jQuery('[data-tab]').each(function (i, el) { new Tab(jQuery(el)); });
+jQuery('[data-faq]').each(function (i, el) { new Faq(jQuery(el)); });
+jQuery('[data-payment-status]').on('click', function () {
+    jQuery('#payment-status-popup').removeClass('d-none').one('click', function (e) {
+        jQuery(e.currentTarget).addClass('d-none');
+    });
 });
-$('[data-view]').on('click', function (e) {
-    var view = jQuery(e.currentTarget).data('view');
-    jQuery('body').load("".concat(view, ".html"));
-});
-// if (typeof window.isWebApp === "boolean" && window.isWebApp) {
-//     const view: string =  window.Telegram.WebApp.initDataUnsafe.start_param
-//     const currentView = window.appView
-//     if (typeof view === "string" && view !== '' && view !== currentView) {
-//         $('body').load(`${view}-app.html`)
-//     }
-//     // if (typeof view === "string" && view !== '') {
-//     //     jQuery.loa
-//     // }
-//     // const view: string =  window.Telegram.WebApp.initDataUnsafe.start_param
-//     // if (typeof view === "string" && view !== '') {
-//     //     jQuery.loa
-//     // }
-// }
